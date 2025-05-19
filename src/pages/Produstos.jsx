ca742d5f98948359c5290f394e84aab8a73bbe75
Ana nobrega <anakarolinefrnobrega@gmail.com>
2025-05-19 16:46:45 -0300

    configuração de filtros

--- src/pages/Produstos.jsx
+++ src/pages/Produstos.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import Produto from "../components/Produto";
+
 const Produtos = () => {
+    const [filtroMarca, setFiltroMarca] = useState([]);
+    const [filtroCategoria, setCategoria] = useState([]);
+    const [filtroGenero, setGenero] = useState([]);
+    const [filtroEstado, setEstado] = useState("novo");
+
+    function verificarMarca(marca) {
+        if (filtroMarca.includes(marca)) {
+            setFiltroMarca([...filtroMarca.filter(cadaMarca => cadaMarca != marca)]);
+        } else {
+            setFiltroMarca([...filtroMarca, marca]);
+        }
+    }
+    console.log(filtroMarca);
+    
+
     return (
         <div className="xl:px-[100px] xl:pt-[40px] xl:pb-[140px]">
             {/* ordenação */}
@@ -18,25 +36,100 @@ const Produtos = () => {
                     <h5 className="font-bold text-grafite">Filtrar por</h5>
                     <div className="h-[1px] bg-cinza-claro my-[20px]"></div>
                     <h6 className="mb-[10px] font-bold text-grafite">Marca</h6>
-                    <div className="grid gap-[10px]"> 
+                    <div className="grid gap-[10px]">
                         <label className="flex gap-[10px] items-center">
-                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            <input 
+                                className="w-[22px] h-[22px] accent-rosa" 
+                                type="checkbox"
+                                onChange={() => verificarMarca("adidas")}
+                                />
                             Adidas
                         </label>
                         <label className="flex gap-[10px] items-center">
-                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            <input 
+                                className="w-[22px] h-[22px] accent-rosa" 
+                                type="checkbox"
+                                onChange={() => verificarMarca("balenciaga")} 
+                                />
                             Balenciaga
                         </label>
                         <label className="flex gap-[10px] items-center">
-                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            <input 
+                                className="w-[22px] h-[22px] accent-rosa" 
+                                type="checkbox"
+                                onChange={() => verificarMarca("nike")} 
+                                />
                             Nike
                         </label>
                         <label className="flex gap-[10px] items-center">
-                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            <input
+                                className="w-[22px] h-[22px] accent-rosa" 
+                                type="checkbox" 
+                                onChange={() => verificarMarca("puma")}
+                                />
                             Puma
                         </label>
                     </div>
+
+                    <h6 className="mb-[10px] font-bold text-grafite  mt-4">Categoria</h6>
+                    <div className="grid gap-[10px]">
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Esporte e Lazer
+                        </label>
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Casual
+                        </label>
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Utilitario
+                        </label>
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Corrido
+                        </label>
+                    </div>
+
+                    <h6 className="mb-[10px] font-bold text-grafite  mt-4">Gênero</h6>
+                    <div className="grid gap-[10px]">
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Masculino
+                        </label>
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Feminino
+                        </label>
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="checkbox" />
+                            Corrida
+                        </label>
+                    </div>
+
+                    <h6 className="mb-[10px] font-bold text-grafite  mt-4">Estado</h6>
+                    <div className="grid gap-[10px]">
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="radio" />
+                            Novo
+                        </label>
+                        <label className="flex gap-[10px] items-center">
+                            <input className="w-[22px] h-[22px] accent-rosa" type="radio" />
+                            usado
+                        </label>
+                    </div>
                 </div>
+
+                {/* Div de produtos */}
+                <div className="grid grid-cols-3 gap[14px]">
+                    <Produto />
+                    <Produto />
+                    <Produto />
+                    <Produto />
+                    <Produto />
+                    <Produto />
+                </div>
+
             </div>
         </div>
     );
